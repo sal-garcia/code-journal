5edbc7f17f69b528fd87ae528a5c39cccd54a2b4
sal garcia <salgarcia940@gmail.com>
2021-09-09 20:12:37 -0700

    Verify that new entries are saved to local storage along with the rest of the data model on page reload. issue#1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 /* global data */
 /* exported data */
-var $input = document.querySelector('#photo-url');
-$input.addEventListener('input', inputfunc);
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
+
+var $photo = document.querySelector('#photo-url');
+$photo.addEventListener('input', inputfunc);
 
 function inputfunc(event) {
-  $input.setAttribute('src', event.target.value);
+  $photo.setAttribute('src', event.target.value);
+
+}
+
+var $form = document.querySelector('#form');
+$form.addEventListener('submit', formFunc);
+
+function formFunc(event) {
+  event.preventDefault();
+  var obj = {
+    title: $title.value,
+    photo: $photo.value,
+    notes: $notes.value,
+    nextEntryId: data.nextEntryId
+  };
+
+  localStorage.setItem('code-journal', JSON.stringify(obj));
+
+  data.entries.unshift(obj);
+  data.nextEntryId++;
+  $photo.setAttribute('src', './images/placeholder-image-square.jpg');
+  $form.reset();
 
 }
